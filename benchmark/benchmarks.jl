88dc36fb842511c23f14dc14cccc9fb5f04b5504
Marco Bonici <58727599+marcobonici@users.noreply.github.com>
2025-06-21 15:58:48 -0400

    Develop (#45)
    * Updated codecov badge
    
    * Adding asv
    
    * Update benchmark.yml
    
    * Update benchmark.yml
    
    * Update benchmark.yml
    
    * Update benchmark.yml
    
    * Update benchmark.yml
    
    * Delete benchmark.yml
    
    * Update benchmark-pr.yml
    
    * More benchmarks
    
    * Update Project.toml
    
    * Update benchmark-pr.yml
    
    * Update benchmark-pr.yml
    
    * Update benchmark-pr.yml
    
    * Update benchmark-pr.yml
    
    * Update benchmark-pr.yml
    
    * Update benchmark-pr.yml
    
    * Fixing dependency issue
    
    * Update benchmark-pr.yml
    
    * Update benchmark-pr.yml
    
    * Update benchmark-pr.yml
    
    * Update benchmark-pr.yml
    
    * Update benchmarks.jl
    
    * Better tests
    
    * Fixing CI
    
    * Update benchmark-pr.yml
    
    * Update benchmarks.jl
    
    * Update benchmarks.jl
    
    * Update benchmark-pr.yml
    
    * Update benchmark-pr.yml
    
    * Update benchmark-pr.yml
    
    * Update benchmark-pr.yml
    
    * Update benchmark-pr.yml
    
    * Update benchmark-pr.yml

---
+++ benchmark/benchmarks.jl
@@ -0,0 +1,59 @@
+#using Pkg
+#Pkg.activate(@__DIR__)
+#Pkg.instantiate()
+
+using BenchmarkTools
+using SimpleChains
+using AbstractCosmologicalEmulators
+using JSON
+
+mlpd = SimpleChain(
+  static(6),
+  TurboDense(tanh, 64),
+  TurboDense(tanh, 64),
+  TurboDense(tanh, 64),
+  TurboDense(tanh, 64),
+  TurboDense(tanh, 64),
+  TurboDense(identity, 40)
+)
+NN_dict = JSON.parsefile(joinpath(@__DIR__, "testNN.json"))
+weights = SimpleChains.init_params(mlpd)
+input = randn(6)
+
+const lx_emu = init_emulator(NN_dict::Dict, weights, AbstractCosmologicalEmulators.LuxEmulator)
+const sc_emu = init_emulator(NN_dict::Dict, weights, AbstractCosmologicalEmulators.SimpleChainsEmulator)
+
+# Every benchmark file must define a BenchmarkGroup named SUITE.
+const SUITE = BenchmarkGroup()
+
+# It's good practice to organize related benchmarks into their own groups.
+# We'll create a group for "normalization" functions.
+SUITE["normalization"] = BenchmarkGroup(["maximin"])
+SUITE["running"] = BenchmarkGroup(["emu"])
+
+# --- Benchmark Setup ---
+# Define the dimensions as constants for consistency.
+const m = 100
+const n = 300
+
+# --- Benchmark Definitions ---
+
+# Benchmark `maximin` on a vector
+SUITE["normalization"]["vector"] = @benchmarkable maximin(x, InMinMax) setup = (
+    InMinMax = hcat(zeros($m), ones($m));
+    x = rand($m)
+)
+
+# Benchmark `maximin` on a matrix
+SUITE["normalization"]["matrix"] = @benchmarkable maximin(y, InMinMax) setup = (
+    InMinMax = hcat(zeros($m), ones($m));
+    y = rand($m, $n)
+)
+
+SUITE["running"]["lux"] = @benchmarkable run_emulator(input, lx_emu) setup = (
+    input = randn(6)
+)
+
+SUITE["running"]["simplechains"] = @benchmarkable run_emulator(input, sc_emu) setup = (
+    input = randn(6)
+)
