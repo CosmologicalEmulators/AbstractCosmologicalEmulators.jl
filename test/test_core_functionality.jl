711f05127d08d9b0d62264b676eeddf1e93d2ea2
Marco Bonici <bonici.marco@gmail.com>
2025-09-10 10:58:08 -0400

    Update test_core_functionality.jl

--- test/test_core_functionality.jl
+++ test/test_core_functionality.jl
@@ -25,9 +25,9 @@ using AbstractCosmologicalEmulators
                                   Description = Dict("emulator_description"=>
                                   NN_dict["emulator_description"]))
 
-    n = 1024
-    A = randn(n)
-    B = ones(n, 2)
+    n_grad = 1024
+    A = randn(n_grad)
+    B = ones(n_grad, 2)
     B[:,1] .*= 0.
     test_sum(A) = sum(abs2, maximin(A, B))
     test_suminv(A) = sum(abs2, inv_maximin(A, B))
@@ -58,23 +58,29 @@ using AbstractCosmologicalEmulators
         @test sc_emu_check.Architecture == sc_emu.Architecture
         @test sc_emu_check.Weights == sc_emu.Weights
         @test sc_emu_check.Description == sc_emu.Description
-        NN_dict["layers"]["layer_1"]["activation_function"]= "adremxud"
-        @test_throws ArgumentError AbstractCosmologicalEmulators._get_nn_simplechains(NN_dict)
-        @test_throws ArgumentError AbstractCosmologicalEmulators._get_nn_lux(NN_dict)
         @test isapprox(run_emulator(input, sc_emu), run_emulator(input, lux_emu))
         @test isapprox(run_emulator(input, lux_emu), run_emulator(stack_input, lux_emu)[:,1])
+        
+        # Test with invalid activation function (use a copy to avoid side effects)
+        NN_dict_invalid = deepcopy(NN_dict)
+        NN_dict_invalid["layers"]["layer_1"]["activation_function"] = "invalid_activation"
+        @test_throws ArgumentError AbstractCosmologicalEmulators._get_nn_simplechains(NN_dict_invalid)
+        @test_throws ArgumentError AbstractCosmologicalEmulators._get_nn_lux(NN_dict_invalid)
     end
 
     @testset "Emulator Description" begin
-        NN_dict = JSON.parsefile(pwd()*"/testNN.json")  # Fresh copy
-        input = randn(6)  # Define input for this test
-        lux_emu = init_emulator(NN_dict, weights, LuxEmulator; validate=false)  # Define lux_emu for this test
+        # Now we can use the original NN_dict since we didn't mutate it
+        input = randn(6)
+        lux_emu = init_emulator(NN_dict, weights, LuxEmulator; validate=false)
         get_emulator_description(NN_dict["emulator_description"])
-        @test_logs (:warn, "We do not know which parameters were included in the emulators training space. Use this trained emulator with caution!") AbstractCosmologicalEmulators.get_emulator_description(Dict("pippo" => "franco"))
+        @test_logs (:warn, "We do not know which parameters were included in the emulators training space. Use this trained emulator with caution!") AbstractCosmologicalEmulators.get_emulator_description(Dict("foo" => "bar"))
         @test isapprox(run_emulator(input, sc_emu), run_emulator(input, lux_emu))
         @test get_emulator_description(sc_emu) == get_emulator_description(NN_dict["emulator_description"])
-        sc_emu.Description = Dict()
-        @test_logs (:warn, "No emulator description found!") AbstractCosmologicalEmulators.get_emulator_description(sc_emu)
+        
+        # Test with missing description (use a copy to avoid affecting sc_emu for other tests)
+        sc_emu_copy = deepcopy(sc_emu)
+        sc_emu_copy.Description = Dict()
+        @test_logs (:warn, "No emulator description found!") AbstractCosmologicalEmulators.get_emulator_description(sc_emu_copy)
     end
 
     @testset "Gradient Tests" begin
