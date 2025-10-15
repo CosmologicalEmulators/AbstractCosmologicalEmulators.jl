d8d89b299117a5e5f96d53a4e5899fafb74e96d7
marcobonici <bonici.marco@gmail.com>
2025-10-15 18:15:49 -0400

    Fixing zygote err

--- ext/BackgroundCosmologyExt/utils.jl
+++ ext/BackgroundCosmologyExt/utils.jl
@@ -1,6 +1,6 @@
 function _transformed_weights(quadrature_rule, order, a, b)
     x, w = quadrature_rule(order)
-    x = (b - a) / 2.0 .* x .+ (b + a) / 2.0
-    w = (b - a) / 2.0 .* w
-    return x, w
+    X = (b - a) / 2.0 .* x .+ (b + a) / 2.0
+    W = (b - a) / 2.0 .* w
+    return X, W
 end
