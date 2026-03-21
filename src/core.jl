4fb12581c50e0baf2fa92a60ca28c24e0109b540
marcobonici <bonici.marco@gmail.com>
2026-03-21 18:22:39 -0400

    Update core, to be reactant compatible

--- src/core.jl
+++ src/core.jl
@@ -43,7 +43,7 @@ function run_emulator(input_params, auxiliary_params, emulator::GenericEmulator)
     norm_output = run_emulator(norm_input, emulator.TrainedEmulator)
 
     # 3. Denormalization
-    output = inv_maximin(Array(norm_output), emulator.OutMinMax)
+    output = inv_maximin(norm_output, emulator.OutMinMax)
 
     # 4. Postprocessing
     result = emulator.Postprocessing(input_params, output, auxiliary_params, emulator)
